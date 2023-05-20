4fe3c7c5e9457965f23084568f30e6beea8ae5ac
George Zhang <george.jx.zhang@gmail.com>
2023-05-20 08:26:33 -0600

    Add site information

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,10 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `wfacechat`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Interviewers APP`,
+    siteUrl: `https://interviewers.mainnote.com`,
+    description: `Revolutionizing Job Interviews with ChatGPT`,
+    image: `src/images/icon.png`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
